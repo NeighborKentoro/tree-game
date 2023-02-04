1bfb8837978dc2aee1e836bc6b13e4d583c9f096
Kent-Desktop\Kent <theswordbattler@gmail.com>
2023-02-04 10:55:16 -0600

    Enemies moving in a pattern

--- Tree Game/Assets/Scripts/Alien.cs
+++ Tree Game/Assets/Scripts/Alien.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour {
 
+    public int movementFramesInterval;
+    public int xLeftBounds;
+    public int xRightBounds;
+
+    private int xDirection;
+    private int xStepsMoved;
+    private int frameCount;
+
     void Start() {
-        
+        this.frameCount = 1;
+        this.xDirection = 1;
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (this.frameCount >= this.movementFramesInterval) {
+            float yStep = 0f;
+            float xStep = 1f * this.xDirection;
+            
+            //only move if so many frames have passed
+            if (xStepsMoved >= xLeftBounds || xStepsMoved <= xRightBounds) {
+                //flip the x movement direction, reset number of x steps moved, increment y position
+                this.xDirection *= -1;
+                this.xStepsMoved = 0;
+                yStep = -1f;
+                xStep = 0f;
+            } else {
+                this.xStepsMoved += 1;
+            }
+            
+            this.transform.position += new Vector3(xStep, yStep, 0);
+            this.frameCount = 1;
+        }
+        this.frameCount++;
     }
 }
