7af7c7339a808862c05b37f137729ddd999b1ed8
nickstrelic <nstrelic@gmail.com>
2023-02-04 13:45:25 -0600

    Root select and shoot from single key

--- Tree Game/Assets/Scripts/Key.cs
+++ Tree Game/Assets/Scripts/Key.cs
@@ -8,32 +8,71 @@ public class Key : MonoBehaviour
     GameObject projectile;
     [SerializeField]
     public GameObject shootPoint;
+    private bool activated = true;
+    private Keyboard keyboard;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        keyboard = GameObject.FindGameObjectWithTag("Keyboard").GetComponent<Keyboard>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-
-            if (hit.collider != null)
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                // spawn projectile
-                Instantiate(projectile, shootPoint.transform);
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+
+                if (hit.collider != null && hit.collider.GetComponent<Key>().activated)
+                {
+                    Debug.Log("Clicked: " + hit.collider.gameObject.tag);
+                    // if in select root mode, this key will be set as root
+                    if (GameObject.FindGameObjectWithTag("Keyboard").GetComponent<Keyboard>().selectRootMode == true)
+                    {
+                        Debug.Log("SELECTING ROOT");
+                        updateRoot(hit.collider.gameObject.tag);
+                    }
+                    // spawn projectile
+                    Instantiate(projectile, hit.collider.GetComponent<Key>().shootPoint.transform);
+                }
+
             }
-            
+        
+        
+    }
+
+    public void deactivate()
+    {
+        activated = false;
+        if (GetComponent<SpriteRenderer>() != null)
+            GetComponent<SpriteRenderer>().color = Color.clear;
+        foreach (Transform c in transform.GetComponentsInChildren<Transform>())
+        {
+            if (c.GetComponent<SpriteRenderer>() != null)
+                c.GetComponent<SpriteRenderer>().color = Color.clear;
         }
     }
 
+    public void activate()
+    {
+        activated = true;
+        if (GetComponent<SpriteRenderer>() != null)
+            GetComponent<SpriteRenderer>().color = Color.blue;
+        // get children
+        foreach (Transform c in transform.GetComponentsInChildren<Transform>())
+        {
+            if (c.GetComponent<SpriteRenderer>() != null)
+                c.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+    }
 
+    private void updateRoot(string tag)
+    {
+        Debug.Log("Setting root to " + tag);
+        keyboard.setRoot(tag);
+    }
 
 }
--- Tree Game/Assets/Scripts/Keyboard.cs
+++ Tree Game/Assets/Scripts/Keyboard.cs
@@ -3,11 +3,158 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Keyboard : MonoBehaviour {
+
+    public string root = "all";
+    public bool selectRootMode = true;
+
     void Start() {
-        
+        Debug.Log(root);
+        Debug.Log(selectRootMode);
+        activateAllKeys();
     }
 
     void Update() {
-        
+
+    }
+
+    public void setRoot(string newRoot)
+    {
+        root = newRoot.ToLower().Trim();
+        setActiveKeyByRoot();
+    }
+
+    private void setActiveKeyByRoot()
+    {
+        GameObject[] keysToActivate;
+        switch (root)
+        {
+            case "a":
+                keysToActivate = GameObject.FindGameObjectsWithTag("a");
+                break;
+            case "a#":
+                keysToActivate = GameObject.FindGameObjectsWithTag("a#");
+                break;
+            case "b":
+                keysToActivate = GameObject.FindGameObjectsWithTag("b");
+                break;
+            case "c":
+                keysToActivate = GameObject.FindGameObjectsWithTag("c");
+                break;
+            case "c#":
+                keysToActivate = GameObject.FindGameObjectsWithTag("c#");
+                break;
+            case "d":
+                keysToActivate = GameObject.FindGameObjectsWithTag("d");
+                break;
+            case "d#":
+                keysToActivate = GameObject.FindGameObjectsWithTag("d#");
+                break;
+            case "e":
+                keysToActivate = GameObject.FindGameObjectsWithTag("e");
+                break;
+            case "f":
+                keysToActivate = GameObject.FindGameObjectsWithTag("f");
+                break;
+            case "f#":
+                keysToActivate = GameObject.FindGameObjectsWithTag("f#");
+                break;
+            case "g":
+                keysToActivate = GameObject.FindGameObjectsWithTag("g");
+                break;
+            case "g#":
+                keysToActivate = GameObject.FindGameObjectsWithTag("g#");
+                break;
+            default:
+                keysToActivate = getAllKeys();
+                break;
+        }
+
+        // deactivate all
+        deactivateAllKeys();
+
+        // enable keys
+        foreach (GameObject key in keysToActivate)
+        {
+            key.GetComponent<Key>().activate();
+            
+        }
+
+        Debug.Log("New root " + root);
+
+        selectRootMode = false;
+    }
+
+    private void deactivateAllKeys()
+    {
+        GameObject[] keys = getAllKeys();
+        foreach(GameObject key in keys)
+        {
+            key.GetComponent<Key>().deactivate();
+        }
+    }
+
+    private void activateAllKeys()
+    {
+        GameObject[] keys = getAllKeys();
+        foreach (GameObject key in keys)
+        {
+            key.GetComponent<Key>().activate();
+        }
+    }
+
+    private GameObject[] getAllKeys()
+    {
+        List<GameObject> keys = new List<GameObject>();
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("a"))
+        {
+            keys.Add(go);
+        }
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("a#"))
+        {
+            keys.Add(go);
+        }
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("b"))
+        {
+            keys.Add(go);
+        }
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("c"))
+        {
+            keys.Add(go);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("c#"))
+        {
+            keys.Add(go);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("d"))
+        {
+            keys.Add(go);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("d#"))
+        {
+            keys.Add(go);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("e"))
+        {
+            keys.Add(go);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("f"))
+        {
+            keys.Add(go);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("f#"))
+        {
+            keys.Add(go);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("g"))
+        {
+            keys.Add(go);
+        }
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("g#"))
+        {
+            keys.Add(go);
+        }
+
+
+        return keys.ToArray();
     }
 }
