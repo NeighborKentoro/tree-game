fa9e64f087ef083170d5b97400c419c5e16b37bb
Kent-Desktop\Kent <theswordbattler@gmail.com>
2023-02-04 22:50:16 -0600

    Added the dynamic music

--- Tree Game/Assets/Scripts/AudioManager.cs
+++ Tree Game/Assets/Scripts/AudioManager.cs
@@ -2,25 +2,22 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
 
-    public AudioSource songAudioSource;
+    public AudioSource[] audioSources;
     public AudioSource drumAudioSource;
-    public AudioClip[] drumLoops;
 
-    private int drumLoopIndex;
     private int tempo;
     private int beat;
     private float secondsPerBeat;
     private float timeElapsed;
 
     void Start() {
-        this.drumLoopIndex = 0;
-        // this.drumAudioSource.clip = this.drumLoops[this.drumLoopIndex];
-        // this.songAudioSource.pitch = 1f;
-        // this.songAudioSource.Play();
-        // this.drumAudioSource.Play();
         this.tempo = 70;
         this.secondsPerBeat = 60f / this.tempo;
         this.timeElapsed = 0f;
+        this.drumAudioSource.Play();
+        foreach (AudioSource src in this.audioSources) {
+            src.Play();
+        }
     }
 
     void Update() {
@@ -54,34 +51,55 @@ public class AudioManager : MonoBehaviour {
         switch (root)
         {
             case "a":
-                
+                this.unmuteTrack(9);
                 break;
             case "a#":
+                this.unmuteTrack(10);
                 break;
             case "b":
+                this.unmuteTrack(11);
                 break;
             case "c":
+                this.unmuteTrack(0);
                 break;
             case "c#":
+                this.unmuteTrack(1);
                 break;
             case "d":
+                this.unmuteTrack(2);
                 break;
             case "d#":
+                this.unmuteTrack(3);
                 break;
             case "e":
+                this.unmuteTrack(4);
                 break;
             case "f":
+                this.unmuteTrack(5);
                 break;
             case "f#":
+                this.unmuteTrack(6);
                 break;
             case "g":
+                this.unmuteTrack(7);
                 break;
             case "g#":
+                this.unmuteTrack(8);
                 break;
             default:
                 break;
         }
-        Debug.Log("Son of a Pitch");
+        
+    }
+
+    private void unmuteTrack(int trackIndex) {
+        for (int i = 0; i < this.audioSources.Length; i++) {
+            if (i == trackIndex) {
+                this.audioSources[trackIndex].mute = false;
+            } else {
+                this.audioSources[i].mute = true;
+            }
+        }
     }
 
 }
